e11d3afc20d5529544062d69d4794d359e612b23
rimma test <valentin.rimma@gmail.com>
2023-05-24 13:27:07 +0400

    add auto fill form

--- src/test/java/ru/netology/selenide/CardDeliveryTest.java
+++ src/test/java/ru/netology/selenide/CardDeliveryTest.java
@@ -3,32 +3,31 @@ package ru.netology.selenide;
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-
-import java.time.*;
+import java.time.Duration;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Configuration.;
+
 
 class CardDeliveryTest {
-    private String generateDate(int addDays, String pattern) {
-      return LocalDate.now(). plusDays(addDays). format(DateTimeFormatter.ofPattern(pattern));
+    public String generateDate(int addDays) {
+      return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
     @Test
     public void shouldCardDeliveryWorks(){
-        
+        String currentData = generateDate(4);
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Санкт-Петербург");
-        String currentData = generateDate(4, "dd.MM.yyyy");
-        $("[data-test-id='data'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
-        $("[data-test-id='data'] input").sendKeys(currentData);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.BACK_SPACE));
+        $("[data-test-id='date'] input").setValue(currentData);
         $("[data-test-id='name'] input").setValue("Куроткин Виктор");
         $("[data-test-id='phone'] input").setValue("+79216556666");
         $("[data-test-id='agreement']").click();
-        $("baton.baton").click();
-        $("notification__").shouldBe(Condition.visible, Duration.ofSeconds(15))
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentData));
 
     }
